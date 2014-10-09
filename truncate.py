ed4b020d1ba7d9d12d2f0ece8989e3de8a832f40
Ilya Isaev <relaxart.ru@gmail.com>
2014-10-09 13:47:57 +0400

    Script for kindle vocabulary truncate

---
+++ truncate.py
@@ -0,0 +1,14 @@
+import config, sqlite3
+
+kindle_config = config.sources.get('kindle')
+conn = sqlite3.connect(kindle_config)
+conn.execute("delete from WORDS;")
+conn.close()
+conn = sqlite3.connect(kindle_config)
+conn = conn.execute("delete from LOOKUPS;")
+conn.close()
+conn = sqlite3.connect(kindle_config)
+conn.execute("update metadata set sscnt = 0 where id in ('WORDS', 'LOOKUPS');")
+conn.close()
+
+
