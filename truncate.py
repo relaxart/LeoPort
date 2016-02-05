662f12179499a0350fb142bd12d78d2e7932e4ed
Ivan Balaksha <tagantroy@gmail.com>
2016-02-05 12:48:30 +0300

    fixed

--- truncate.py
+++ truncate.py
@@ -7,13 +7,13 @@ kindle_config = config.sources.get('kindle')
 
 conn = sqlite3.connect(kindle_config)
 cursor = conn.cursor()
-cursor.execute("DELETE FROM WORDS;")
+cursor.execute("delete from WORDS;")
 conn.commit()
 cursor.close()
 
 conn = sqlite3.connect(kindle_config)
 cursor = conn.cursor()
-cursor.execute("DELETE FROM LOOKUPS;")
+cursor.execute("delete from LOOKUPS;")
 conn.commit()
 cursor.close()
 
